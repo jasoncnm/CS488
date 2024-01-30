b94177e4740d2ddbd8ab505ed07b0ee2eec2026e
mjunjie <mjunjie@gl37.student.cs.uwaterloo.ca>
2024-01-30 15:41:39 -0500

    a2:start

--- A2/A2.cpp
+++ A2/A2.cpp
@@ -16,20 +16,39 @@ using namespace glm;
 //----------------------------------------------------------------------------------------
 // Constructor
 VertexData::VertexData()
-	: numVertices(0),
-	  index(0)
+    : numVertices(0),
+      index(0)
 {
-	positions.resize(kMaxVertices);
-	colours.resize(kMaxVertices);
+    positions.resize(kMaxVertices);
+    colours.resize(kMaxVertices);
 }
 
 
 //----------------------------------------------------------------------------------------
 // Constructor
-A2::A2()
-	: m_currentLineColour(vec3(0.0f))
+A2::A2() : m_currentLineColour(vec3(0.0f))
 {
+    cube[0] = vec3(-1, -1, -1);
+    cube[1] = vec3( 1, -1, -1);
+    cube[2] = vec3(-1,  1, -1);
+    cube[3] = vec3(-1, -1,  1);
+    cube[4] = vec3( 1,  1, -1);
+    cube[5] = vec3( 1, -1,  1);
+    cube[6] = vec3(-1,  1,  1);
+    cube[7] = vec3( 1,  1,  1);
 
+    cubeLines[0] = vec2(0, 1);
+    cubeLines[1] = vec2(1, 5);
+    cubeLines[2] = vec2(5, 3);
+    cubeLines[3] = vec2(3, 0);
+    cubeLines[4] = vec2(0, 2);
+    cubeLines[5] = vec2(1, 4);
+    cubeLines[6] = vec2(5, 7);
+    cubeLines[7] = vec2(3, 6);
+    cubeLines[8] = vec2(2, 4);
+    cubeLines[9] = vec2(4, 7);
+    cubeLines[10] = vec2(7, 6);
+    cubeLines[11] = vec2(6, 2);
 }
 
 //----------------------------------------------------------------------------------------
@@ -45,139 +64,139 @@ A2::~A2()
  */
 void A2::init()
 {
-	// Set the background colour.
-	glClearColor(0.3, 0.5, 0.7, 1.0);
+    // Set the background colour.
+    glClearColor(0.3, 0.5, 0.7, 1.0);
 
-	createShaderProgram();
+    createShaderProgram();
 
-	glGenVertexArrays(1, &m_vao);
+    glGenVertexArrays(1, &m_vao);
 
-	enableVertexAttribIndices();
+    enableVertexAttribIndices();
 
-	generateVertexBuffers();
+    generateVertexBuffers();
 
-	mapVboDataToVertexAttributeLocation();
+    mapVboDataToVertexAttributeLocation();
 }
 
 //----------------------------------------------------------------------------------------
 void A2::createShaderProgram()
 {
-	m_shader.generateProgramObject();
-	m_shader.attachVertexShader( getAssetFilePath("VertexShader.vs").c_str() );
-	m_shader.attachFragmentShader( getAssetFilePath("FragmentShader.fs").c_str() );
-	m_shader.link();
+    m_shader.generateProgramObject();
+    m_shader.attachVertexShader( getAssetFilePath("VertexShader.vs").c_str() );
+    m_shader.attachFragmentShader( getAssetFilePath("FragmentShader.fs").c_str() );
+    m_shader.link();
 }
 
 //---------------------------------------------------------------------------------------- Spring 2020
 void A2::enableVertexAttribIndices()
 {
-	glBindVertexArray(m_vao);
+    glBindVertexArray(m_vao);
 
-	// Enable the attribute index location for "position" when rendering.
-	GLint positionAttribLocation = m_shader.getAttribLocation( "position" );
-	glEnableVertexAttribArray(positionAttribLocation);
+    // Enable the attribute index location for "position" when rendering.
+    GLint positionAttribLocation = m_shader.getAttribLocation( "position" );
+    glEnableVertexAttribArray(positionAttribLocation);
 
-	// Enable the attribute index location for "colour" when rendering.
-	GLint colourAttribLocation = m_shader.getAttribLocation( "colour" );
-	glEnableVertexAttribArray(colourAttribLocation);
+    // Enable the attribute index location for "colour" when rendering.
+    GLint colourAttribLocation = m_shader.getAttribLocation( "colour" );
+    glEnableVertexAttribArray(colourAttribLocation);
 
-	// Restore defaults
-	glBindVertexArray(0);
+    // Restore defaults
+    glBindVertexArray(0);
 
-	CHECK_GL_ERRORS;
+    CHECK_GL_ERRORS;
 }
 
 //----------------------------------------------------------------------------------------
 void A2::generateVertexBuffers()
 {
-	// Generate a vertex buffer to store line vertex positions
-	{
-		glGenBuffers(1, &m_vbo_positions);
+    // Generate a vertex buffer to store line vertex positions
+    {
+        glGenBuffers(1, &m_vbo_positions);
 
-		glBindBuffer(GL_ARRAY_BUFFER, m_vbo_positions);
+        glBindBuffer(GL_ARRAY_BUFFER, m_vbo_positions);
 
-		// Set to GL_DYNAMIC_DRAW because the data store will be modified frequently.
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vec2) * kMaxVertices, nullptr,
-				GL_DYNAMIC_DRAW);
-
-
-		// Unbind the target GL_ARRAY_BUFFER, now that we are finished using it.
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-		CHECK_GL_ERRORS;
-	}
-
-	// Generate a vertex buffer to store line colors
-	{
-		glGenBuffers(1, &m_vbo_colours);
-
-		glBindBuffer(GL_ARRAY_BUFFER, m_vbo_colours);
-
-		// Set to GL_DYNAMIC_DRAW because the data store will be modified frequently.
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vec3) * kMaxVertices, nullptr,
-				GL_DYNAMIC_DRAW);
+        // Set to GL_DYNAMIC_DRAW because the data store will be modified frequently.
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vec2) * kMaxVertices, nullptr,
+                GL_DYNAMIC_DRAW);
 
 
-		// Unbind the target GL_ARRAY_BUFFER, now that we are finished using it.
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // Unbind the target GL_ARRAY_BUFFER, now that we are finished using it.
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-		CHECK_GL_ERRORS;
-	}
+        CHECK_GL_ERRORS;
+    }
+
+    // Generate a vertex buffer to store line colors
+    {
+        glGenBuffers(1, &m_vbo_colours);
+
+        glBindBuffer(GL_ARRAY_BUFFER, m_vbo_colours);
+
+        // Set to GL_DYNAMIC_DRAW because the data store will be modified frequently.
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vec3) * kMaxVertices, nullptr,
+                GL_DYNAMIC_DRAW);
+
+
+        // Unbind the target GL_ARRAY_BUFFER, now that we are finished using it.
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        CHECK_GL_ERRORS;
+    }
 }
 
 //----------------------------------------------------------------------------------------
 void A2::mapVboDataToVertexAttributeLocation()
 {
-	// Bind VAO in order to record the data mapping.
-	glBindVertexArray(m_vao);
+    // Bind VAO in order to record the data mapping.
+    glBindVertexArray(m_vao);
 
-	// Tell GL how to map data from the vertex buffer "m_vbo_positions" into the
-	// "position" vertex attribute index for any bound shader program.
-	glBindBuffer(GL_ARRAY_BUFFER, m_vbo_positions);
-	GLint positionAttribLocation = m_shader.getAttribLocation( "position" );
-	glVertexAttribPointer(positionAttribLocation, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+    // Tell GL how to map data from the vertex buffer "m_vbo_positions" into the
+    // "position" vertex attribute index for any bound shader program.
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo_positions);
+    GLint positionAttribLocation = m_shader.getAttribLocation( "position" );
+    glVertexAttribPointer(positionAttribLocation, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-	// Tell GL how to map data from the vertex buffer "m_vbo_colours" into the
-	// "colour" vertex attribute index for any bound shader program.
-	glBindBuffer(GL_ARRAY_BUFFER, m_vbo_colours);
-	GLint colorAttribLocation = m_shader.getAttribLocation( "colour" );
-	glVertexAttribPointer(colorAttribLocation, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+    // Tell GL how to map data from the vertex buffer "m_vbo_colours" into the
+    // "colour" vertex attribute index for any bound shader program.
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo_colours);
+    GLint colorAttribLocation = m_shader.getAttribLocation( "colour" );
+    glVertexAttribPointer(colorAttribLocation, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-	//-- Unbind target, and restore default values:
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+    //-- Unbind target, and restore default values:
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
-	CHECK_GL_ERRORS;
+    CHECK_GL_ERRORS;
 }
 
 //---------------------------------------------------------------------------------------
 void A2::initLineData()
 {
-	m_vertexData.numVertices = 0;
-	m_vertexData.index = 0;
+    m_vertexData.numVertices = 0;
+    m_vertexData.index = 0;
 }
 
 //---------------------------------------------------------------------------------------
 void A2::setLineColour (
-		const glm::vec3 & colour
+        const glm::vec3 & colour
 ) {
-	m_currentLineColour = colour;
+    m_currentLineColour = colour;
 }
 
 //---------------------------------------------------------------------------------------
 void A2::drawLine(
-		const glm::vec2 & V0,   // Line Start (NDC coordinate)
-		const glm::vec2 & V1    // Line End (NDC coordinate)
+        const glm::vec2 & V0,   // Line Start (NDC coordinate)
+        const glm::vec2 & V1    // Line End (NDC coordinate)
 ) {
 
-	m_vertexData.positions[m_vertexData.index] = V0;
-	m_vertexData.colours[m_vertexData.index] = m_currentLineColour;
-	++m_vertexData.index;
-	m_vertexData.positions[m_vertexData.index] = V1;
-	m_vertexData.colours[m_vertexData.index] = m_currentLineColour;
-	++m_vertexData.index;
+    m_vertexData.positions[m_vertexData.index] = V0;
+    m_vertexData.colours[m_vertexData.index] = m_currentLineColour;
+    ++m_vertexData.index;
+    m_vertexData.positions[m_vertexData.index] = V1;
+    m_vertexData.colours[m_vertexData.index] = m_currentLineColour;
+    ++m_vertexData.index;
 
-	m_vertexData.numVertices += 2;
+    m_vertexData.numVertices += 2;
 }
 
 //----------------------------------------------------------------------------------------
@@ -186,25 +205,25 @@ void A2::drawLine(
  */
 void A2::appLogic()
 {
-	// Place per frame, application logic here ...
+    // Place per frame, application logic here ...
 
-	// Call at the beginning of frame, before drawing lines:
-	initLineData();
+    // Call at the beginning of frame, before drawing lines:
+    initLineData();
 
-	// Draw outer square:
-	setLineColour(vec3(1.0f, 0.7f, 0.8f));
-	drawLine(vec2(-0.5f, -0.5f), vec2(0.5f, -0.5f));
-	drawLine(vec2(0.5f, -0.5f), vec2(0.5f, 0.5f));
-	drawLine(vec2(0.5f, 0.5f), vec2(-0.5f, 0.5f));
-	drawLine(vec2(-0.5f, 0.5f), vec2(-0.5f, -0.5f));
+    // Draw outer square:
+    setLineColour(vec3(1.0f, 0.7f, 0.8f));
+    drawLine(vec2(-0.5f, -0.5f), vec2(0.5f, -0.5f));
+    drawLine(vec2(0.5f, -0.5f), vec2(0.5f, 0.5f));
+    drawLine(vec2(0.5f, 0.5f), vec2(-0.5f, 0.5f));
+    drawLine(vec2(-0.5f, 0.5f), vec2(-0.5f, -0.5f));
 
 
-	// Draw inner square:
-	setLineColour(vec3(0.2f, 1.0f, 1.0f));
-	drawLine(vec2(-0.25f, -0.25f), vec2(0.25f, -0.25f));
-	drawLine(vec2(0.25f, -0.25f), vec2(0.25f, 0.25f));
-	drawLine(vec2(0.25f, 0.25f), vec2(-0.25f, 0.25f));
-	drawLine(vec2(-0.25f, 0.25f), vec2(-0.25f, -0.25f));
+    // Draw inner square:
+    setLineColour(vec3(0.2f, 1.0f, 1.0f));
+    drawLine(vec2(-0.25f, -0.25f), vec2(0.25f, -0.25f));
+    drawLine(vec2(0.25f, -0.25f), vec2(0.25f, 0.25f));
+    drawLine(vec2(0.25f, 0.25f), vec2(-0.25f, 0.25f));
+    drawLine(vec2(-0.25f, 0.25f), vec2(-0.25f, -0.25f));
 }
 
 //----------------------------------------------------------------------------------------
@@ -213,55 +232,55 @@ void A2::appLogic()
  */
 void A2::guiLogic()
 {
-	static bool firstRun(true);
-	if (firstRun) {
-		ImGui::SetNextWindowPos(ImVec2(50, 50));
-		firstRun = false;
-	}
+    static bool firstRun(true);
+    if (firstRun) {
+        ImGui::SetNextWindowPos(ImVec2(50, 50));
+        firstRun = false;
+    }
 
-	static bool showDebugWindow(true);
-	ImGuiWindowFlags windowFlags(ImGuiWindowFlags_AlwaysAutoResize);
-	float opacity(0.5f);
+    static bool showDebugWindow(true);
+    ImGuiWindowFlags windowFlags(ImGuiWindowFlags_AlwaysAutoResize);
+    float opacity(0.5f);
 
-	ImGui::Begin("Properties", &showDebugWindow, ImVec2(100,100), opacity,
-			windowFlags);
-
-
-		// Add more gui elements here here ...
+    ImGui::Begin("Properties", &showDebugWindow, ImVec2(100,100), opacity,
+            windowFlags);
 
 
-		// Create Button, and check if it was clicked:
-		if( ImGui::Button( "Quit Application" ) ) {
-			glfwSetWindowShouldClose(m_window, GL_TRUE);
-		}
+        // Add more gui elements here here ...
 
-		ImGui::Text( "Framerate: %.1f FPS", ImGui::GetIO().Framerate );
 
-	ImGui::End();
+        // Create Button, and check if it was clicked:
+        if( ImGui::Button( "Quit Application" ) ) {
+            glfwSetWindowShouldClose(m_window, GL_TRUE);
+        }
+
+        ImGui::Text( "Framerate: %.1f FPS", ImGui::GetIO().Framerate );
+
+    ImGui::End();
 }
 
 //----------------------------------------------------------------------------------------
 void A2::uploadVertexDataToVbos() {
 
-	//-- Copy vertex position data into VBO, m_vbo_positions:
-	{
-		glBindBuffer(GL_ARRAY_BUFFER, m_vbo_positions);
-		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vec2) * m_vertexData.numVertices,
-				m_vertexData.positions.data());
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
+    //-- Copy vertex position data into VBO, m_vbo_positions:
+    {
+        glBindBuffer(GL_ARRAY_BUFFER, m_vbo_positions);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vec2) * m_vertexData.numVertices,
+                m_vertexData.positions.data());
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-		CHECK_GL_ERRORS;
-	}
+        CHECK_GL_ERRORS;
+    }
 
-	//-- Copy vertex colour data into VBO, m_vbo_colours:
-	{
-		glBindBuffer(GL_ARRAY_BUFFER, m_vbo_colours);
-		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vec3) * m_vertexData.numVertices,
-				m_vertexData.colours.data());
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
+    //-- Copy vertex colour data into VBO, m_vbo_colours:
+    {
+        glBindBuffer(GL_ARRAY_BUFFER, m_vbo_colours);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vec3) * m_vertexData.numVertices,
+                m_vertexData.colours.data());
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-		CHECK_GL_ERRORS;
-	}
+        CHECK_GL_ERRORS;
+    }
 }
 
 //----------------------------------------------------------------------------------------
@@ -270,18 +289,18 @@ void A2::uploadVertexDataToVbos() {
  */
 void A2::draw()
 {
-	uploadVertexDataToVbos();
+    uploadVertexDataToVbos();
 
-	glBindVertexArray(m_vao);
+    glBindVertexArray(m_vao);
 
-	m_shader.enable();
-		glDrawArrays(GL_LINES, 0, m_vertexData.numVertices);
-	m_shader.disable();
+    m_shader.enable();
+        glDrawArrays(GL_LINES, 0, m_vertexData.numVertices);
+    m_shader.disable();
 
-	// Restore defaults
-	glBindVertexArray(0);
+    // Restore defaults
+    glBindVertexArray(0);
 
-	CHECK_GL_ERRORS;
+    CHECK_GL_ERRORS;
 }
 
 //----------------------------------------------------------------------------------------
@@ -298,13 +317,13 @@ void A2::cleanup()
  * Event handler.  Handles cursor entering the window area events.
  */
 bool A2::cursorEnterWindowEvent (
-		int entered
+        int entered
 ) {
-	bool eventHandled(false);
+    bool eventHandled(false);
 
-	// Fill in with event handling code...
+    // Fill in with event handling code...
 
-	return eventHandled;
+    return eventHandled;
 }
 
 //----------------------------------------------------------------------------------------
@@ -312,14 +331,14 @@ bool A2::cursorEnterWindowEvent (
  * Event handler.  Handles mouse cursor movement events.
  */
 bool A2::mouseMoveEvent (
-		double xPos,
-		double yPos
+        double xPos,
+        double yPos
 ) {
-	bool eventHandled(false);
+    bool eventHandled(false);
 
-	// Fill in with event handling code...
+    // Fill in with event handling code...
 
-	return eventHandled;
+    return eventHandled;
 }
 
 //----------------------------------------------------------------------------------------
@@ -327,15 +346,15 @@ bool A2::mouseMoveEvent (
  * Event handler.  Handles mouse button events.
  */
 bool A2::mouseButtonInputEvent (
-		int button,
-		int actions,
-		int mods
+        int button,
+        int actions,
+        int mods
 ) {
-	bool eventHandled(false);
+    bool eventHandled(false);
 
-	// Fill in with event handling code...
+    // Fill in with event handling code...
 
-	return eventHandled;
+    return eventHandled;
 }
 
 //----------------------------------------------------------------------------------------
@@ -343,14 +362,14 @@ bool A2::mouseButtonInputEvent (
  * Event handler.  Handles mouse scroll wheel events.
  */
 bool A2::mouseScrollEvent (
-		double xOffSet,
-		double yOffSet
+        double xOffSet,
+        double yOffSet
 ) {
-	bool eventHandled(false);
+    bool eventHandled(false);
 
-	// Fill in with event handling code...
+    // Fill in with event handling code...
 
-	return eventHandled;
+    return eventHandled;
 }
 
 //----------------------------------------------------------------------------------------
@@ -358,14 +377,14 @@ bool A2::mouseScrollEvent (
  * Event handler.  Handles window resize events.
  */
 bool A2::windowResizeEvent (
-		int width,
-		int height
+        int width,
+        int height
 ) {
-	bool eventHandled(false);
+    bool eventHandled(false);
 
-	// Fill in with event handling code...
+    // Fill in with event handling code...
 
-	return eventHandled;
+    return eventHandled;
 }
 
 //----------------------------------------------------------------------------------------
@@ -373,13 +392,13 @@ bool A2::windowResizeEvent (
  * Event handler.  Handles key input events.
  */
 bool A2::keyInputEvent (
-		int key,
-		int action,
-		int mods
+        int key,
+        int action,
+        int mods
 ) {
-	bool eventHandled(false);
+    bool eventHandled(false);
 
-	// Fill in with event handling code...
+    // Fill in with event handling code...
 
-	return eventHandled;
+    return eventHandled;
 }
--- A2/A2.hpp
+++ A2/A2.hpp
@@ -19,57 +19,58 @@ const GLsizei kMaxVertices = 1000;
 // Data should be copied over to GPU memory via VBO storage before rendering.
 class VertexData {
 public:
-	VertexData();
+    VertexData();
 
-	std::vector<glm::vec2> positions;
-	std::vector<glm::vec3> colours;
-	GLuint index;
-	GLsizei numVertices;
+    std::vector<glm::vec2> positions;
+    std::vector<glm::vec3> colours;
+    GLuint index;
+    GLsizei numVertices;
 };
 
 
 class A2 : public CS488Window {
 public:
-	A2();
-	virtual ~A2();
+    A2();
+    virtual ~A2();
 
 protected:
-	virtual void init() override;
-	virtual void appLogic() override;
-	virtual void guiLogic() override;
-	virtual void draw() override;
-	virtual void cleanup() override;
+    virtual void init() override;
+    virtual void appLogic() override;
+    virtual void guiLogic() override;
+    virtual void draw() override;
+    virtual void cleanup() override;
 
-	virtual bool cursorEnterWindowEvent(int entered) override;
-	virtual bool mouseMoveEvent(double xPos, double yPos) override;
-	virtual bool mouseButtonInputEvent(int button, int actions, int mods) override;
-	virtual bool mouseScrollEvent(double xOffSet, double yOffSet) override;
-	virtual bool windowResizeEvent(int width, int height) override;
-	virtual bool keyInputEvent(int key, int action, int mods) override;
+    virtual bool cursorEnterWindowEvent(int entered) override;
+    virtual bool mouseMoveEvent(double xPos, double yPos) override;
+    virtual bool mouseButtonInputEvent(int button, int actions, int mods) override;
+    virtual bool mouseScrollEvent(double xOffSet, double yOffSet) override;
+    virtual bool windowResizeEvent(int width, int height) override;
+    virtual bool keyInputEvent(int key, int action, int mods) override;
 
-	void createShaderProgram();
-	void enableVertexAttribIndices();
-	void generateVertexBuffers();
-	void mapVboDataToVertexAttributeLocation();
-	void uploadVertexDataToVbos();
+    void createShaderProgram();
+    void enableVertexAttribIndices();
+    void generateVertexBuffers();
+    void mapVboDataToVertexAttributeLocation();
+    void uploadVertexDataToVbos();
 
-	void initLineData();
+    void initLineData();
 
-	void setLineColour(const glm::vec3 & colour);
+    void setLineColour(const glm::vec3 & colour);
 
-	void drawLine (
-			const glm::vec2 & v0,
-			const glm::vec2 & v1
-	);
+    void drawLine (
+            const glm::vec2 & v0,
+            const glm::vec2 & v1
+    );
 
-	ShaderProgram m_shader;
+    ShaderProgram m_shader;
 
-	GLuint m_vao;            // Vertex Array Object
-	GLuint m_vbo_positions;  // Vertex Buffer Object
-	GLuint m_vbo_colours;    // Vertex Buffer Object
+    GLuint m_vao;            // Vertex Array Object
+    GLuint m_vbo_positions;  // Vertex Buffer Object
+    GLuint m_vbo_colours;    // Vertex Buffer Object
 
-	VertexData m_vertexData;
+    VertexData m_vertexData;
 
-	glm::vec3 m_currentLineColour;
-
+    glm::vec3 m_currentLineColour;
+    glm::vec3 cube[8];
+    glm::vec2 cubeLines[12];
 };
